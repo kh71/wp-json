3d1466986e5975b1f65c35a43cbd655c12b8dc70
Dara <dara@kh71.com>
2019-09-19 18:17:55 +0700

    update test

--- test/posts.js
+++ test/posts.js
@@ -1,9 +1,9 @@
 const WPAPI = require("../index.js");
 async function run1() {
   const wpJson = new WPAPI("postnews.com.kh");
-  const count = await wpJson.counter(485979).then(res => { return res; });
-  console.log("Post 485979: ", res);
   const res = await wpJson.post(485979).then(res => { return res; });
+  console.log("Post 485979: ", res);
+  const count = await wpJson.counter(485979).then(res => { return res; });
   console.log("Update Count:", count);
 };
 
