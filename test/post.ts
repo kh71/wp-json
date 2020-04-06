ee9348cd900f0044c0781cfda36d21aeaacc6a9f
Dara <dara@kh71.com>
2020-04-06 13:54:01 +0700

    update package

--- test/post.ts
+++ test/post.ts
@@ -2,7 +2,7 @@ import WPAPI from "../dist";
 // var WPAPI = require("../dist");
 async function run1() {
     const wpJson = new WPAPI("postnews.com.kh");
-    const res = await wpJson.categories().then(res => { return res; });
+    const res = await wpJson.tags(1, 100).then(res => { return res; });
     console.log(res);
 }
 
